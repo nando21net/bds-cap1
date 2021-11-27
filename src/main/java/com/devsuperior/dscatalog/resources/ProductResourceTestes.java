be19360ee67fb105b552a316329f64d61f41c90d
Fernando Dias Seixas <nando21.net@gmail.com>
2021-11-27 19:04:57 -0300

    Junit Camada Web

---
+++ src/main/java/com/devsuperior/dscatalog/resources/ProductResourceTestes.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dscatalog.resources;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(ProductResource.class)
+public class ProductResourceTestes {
+
+}
